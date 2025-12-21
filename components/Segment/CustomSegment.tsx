094212d8f4362aab096cef7267070783b74c04ec
discordaccont78-create <discordaccont78@gmail.com>
2025-12-21 17:35:16 +0330

    feat: Update Slider import path
    Relocates the Slider component import from `../UI/Slider` to `../ui/slider` to align with the new project structure and component organization. This change is a refactor to improve maintainability.

--- components/Segment/CustomSegment.tsx
+++ components/Segment/CustomSegment.tsx
@@ -3,10 +3,10 @@ import { motion } from 'framer-motion';
 import { Power, Send, Trash2, Clock, Hourglass, Settings2, MousePointerClick, Fingerprint } from 'lucide-react';
 import { Button } from '../ui/button';
 import { Input } from '../ui/input';
-import { Slider } from '../UI/Slider';
+import { Slider } from '../ui/slider';
 import { Switch } from '../ui/switch';
 import { Segment, CMD } from '../../types/index';
-import { useDeviceState } from '../../hooks/useDevice';
+import { useDeviceState, useDeviceControl } from '../../hooks/useDevice';
 import { useSegments } from '../../lib/store/segments';
 import { cn } from '../../lib/utils';
 
